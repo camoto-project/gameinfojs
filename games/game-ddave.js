4391551c6cd3b22a948aa5ecec58464337688a0a
Adam Nielsen <malvineous@shikadi.net>
2021-03-27 02:03:12 +1000

    game-ddave: Merge masks into main image as palette transparency
    Still need to finalise the difference between CGA, EGA and VGA tilesets.

--- games/game-ddave.js
+++ games/game-ddave.js
@@ -30,6 +30,8 @@ import { arc_exe_ddave } from '@camoto/gamearchive';
 import {
 	imageFromTileset,
 	tilesetFromImage,
+	imageFromMask,
+	maskFromImage,
 	pal_vga_6bit,
 	tls_ddave_cga,
 	tls_ddave_ega,
@@ -144,7 +146,7 @@ export default class Game_DDave extends Game
 			}
 		}
 
-		const filePalVGA = this.exe.files.find(f => f.name.toUpperCase() === 'VGA.PAL');
+		const filePalVGA = this.exe.files.find(f => f.name.toLowerCase() === 'vga.pal');
 		this.palVGA = pal_vga_6bit.read({
 			main: filePalVGA.getContent(),
 		});
@@ -184,38 +186,174 @@ export default class Game_DDave extends Game
 			};
 		}
 
-		const gfxSources = ['cga', 'ega', 'vga'];
 		let gfx = {};
-		for (const xga of gfxSources) {
+
+		const palIndexTransparent = {
+			cga: [3, 4],      // one more than full palette
+			ega: [15, 16],    // one more than full palette
+			vga: [255, 230],  // random colour that isn't used in the original game
+		};
+
+		const addTiles = (title, xga, offset, count, width, masked = false) => {
 			let itmTiles = {
-				title: `Map tiles (${xga.toUpperCase()})`,
+				title: title,
 				type: Game.ItemTypes.Image,
 			};
 			if (!this.tileset[xga]) {
 				// Tileset wasn't loaded, disable opening.
 				itmTiles.disabled = true;
+			} else if (count === 1) {
+				itmTiles.fnOpen = () => {
+					let imgTileset = this.tileset[xga][offset];
+					if (xga === 'vga') {
+						imgTileset.palette = this.palVGA[0].palette;
+					} else {
+						// Copy the default CGA/EGA palette.
+						imgTileset.palette = this.tileset[xga][0].palette;
+					}
+					return [ imgTileset ];
+				};
+				itmTiles.fnSave = obj => {
+					this.tileset[xga].splice(offset, offset + count, obj[0]);
+					return {
+						warnings: [],
+					};
+				};
 			} else {
 				// Function to open the file and return an Array<Image>.
 				itmTiles.fnOpen = () => {
-					// Extract the map tiles from the tileset and turn it into an image.
-					let imgTileset = imageFromTileset(this.tileset[xga].slice(0, 53), 4);
+					let imgTileset;
+					if (masked) {
+						// These tiles are masked, so convert them into a single image with
+						// a transparent palette entry used instead of the mask.
+						const outputTiles = [];
+						let numImages = width;
+						const tiles = this.tileset[xga].slice(offset, offset + numImages);
+						const masks = this.tileset[xga].slice(offset + numImages, offset + numImages * 2);
+						const [ piMask, piTrans ] = palIndexTransparent[xga];
+						for (let i = 0; i < numImages; i++) {
+							outputTiles.push(
+								imageFromMask({
+									imgVisible: tiles[i],
+									imgMask: masks[i],
+									cb: (v, m) => (m === piMask) ? piTrans : v,
+								})
+							);
+						}
+						imgTileset = imageFromTileset(outputTiles, width);
+					} else {
+						// Extract the map tiles from the tileset and turn it into an image.
+						imgTileset = imageFromTileset(this.tileset[xga].slice(offset, offset + count), width);
+					}
+
 					if (xga === 'vga') {
 						imgTileset.palette = this.palVGA[0].palette;
+					} else {
+						// Copy the default CGA/EGA palette.
+						imgTileset.palette = this.tileset[xga][0].palette;
 					}
+
+					// Create a transparent entry in the palette.
+					if (masked) {
+						if (xga === 'vga') {
+							imgTileset.palette = imgTileset.palette.clone();
+							imgTileset.palette[palIndexTransparent] = [0xFF, 0x00, 0xFF, 0x00];
+						} else {
+							// For CGA and EGA, add an extra palette entry for transparency.
+							imgTileset.palette = imgTileset.palette.clone();
+							imgTileset.palette.push([0xFF, 0x00, 0xFF, 0x00]);
+						}
+					}
+
 					return [ imgTileset ];
 				};
 				itmTiles.fnSave = obj => {
 					// Convert the Image instance back into individual tiles and store
 					// them in the loaded tileset.
-					const tiles = tilesetFromImage(obj[0], {x: 16, y: 16}, 53, 0);
-					// Replace the first 53 tiles with the new list.
-					this.tileset[xga].splice(0, 53, ...tiles);
+					let tiles;
+					if (masked) {
+						// Convert the transparent pixels back to a mask image.
+						const origTiles = this.tileset[xga].slice(offset, offset + width);
+						let inputTiles = tilesetFromImage(obj[0], origTiles, 0);
+						let clearTiles = [];
+						let masks = [];
+						for (let i = 0; i < width; i++) {
+							const { imgVisible, imgMask } = maskFromImage({
+								img: inputTiles[i],
+								cb: p => {
+									const alpha = inputTiles[i].palette[p][3] === 0x00;
+									return [
+										// Pass the pixel through unchanged, unless it's transparent
+										// in which case use palette entry 0 instead.
+										alpha ? 0x00 : p,
+										// Set the mask pixel to palette entry 255 if it should be
+										// transparent, otherwise use 0.
+										alpha ? 0xFF : 0x00,
+									];
+								},
+							});
+							clearTiles.push(imgVisible);
+							masks.push(imgMask);
+						}
+						// Put all the visible tiles first, followed by all the masks.
+						tiles = [ ...clearTiles, ...masks ];
+					} else {
+						const origTiles = this.tileset[xga].slice(offset, offset + count);
+						tiles = tilesetFromImage(obj[0], origTiles, 0);
+					}
+					// Replace a section of tiles within the tileset with the new list.
+					debug('before', this.tileset[xga].length, offset, count, tiles.length);
+					this.tileset[xga].splice(offset, count, ...tiles);
+					debug('after', this.tileset[xga].length);
 					return {
 						warnings: [],
 					};
 				};
 			}
-			gfx[`tiles-${xga}`] = itmTiles;
+			return itmTiles;
+		}
+		// These ones are the same for all.
+		const gfxSources = ['cga', 'ega', 'vga'];
+		for (const xga of gfxSources) {
+			gfx[`${xga}-tiles`] = addTiles(`Map tiles (${xga.toUpperCase()})`, xga, 0, 53, 9);
+		}
+		const vgaTiles = [
+			['walk', 'Walking', 14, 7, true],
+			['jump', 'Jumping', 4, 2, true],
+			['climb', 'Climbing', 6, 3, true],
+			['fly', 'Flying', 12, 6, true],
+			['m1', 'Monster 1', 4, 4],
+			['m2', 'Monster 2', 4, 4],
+			['m3', 'Monster 3', 4, 4],
+			['m4', 'Monster 4', 4, 4],
+			['m5', 'Monster 5', 4, 4],
+			['m6', 'Monster 6', 4, 4],
+			['m7', 'Monster 7', 4, 4],
+			['m8', 'Monster 8', 4, 4],
+			['mb', 'Monster Bullet', 6, 6],
+			['bullet', 'Bullet', 2, 2],
+			['explode', 'Explosion', 4, 4],
+			['inv-jetpack', 'Inventory - Jetpack', 1, 1],
+			['inv-gun', 'Inventory - Gun', 1, 1],
+			['stat-lives', 'Status - Lives', 1, 1],
+			['stat-level', 'Status - Level', 1, 1],
+			['stat-score', 'Status - Score', 1, 1],
+			['inv-door', 'Inventory - Door', 1, 1],
+			['msg-warp', 'Message - Warp', 1, 1],
+			['msg-zone', 'Message - Zone', 1, 1],
+			['inv-fuel', 'Inventory - Jetpack fuel', 1, 1],
+			['inv-fuel-bar', 'Inventory - Jetpack fuel bar', 1, 1],
+			['stat-life', 'Status - Life icon', 1, 1],
+			['title', 'Title animation', 4, 1],
+			['font-numbers', 'Font (numbers)', 10, 10],
+		];
+		let nextTileVGA = 53, nextTileCEGA = 53;
+		for (const t of vgaTiles) {
+			gfx[`vga-${t[0]}`] = addTiles(t[1], 'vga', nextTileVGA, t[2], t[3], t[4]);
+			gfx[`ega-${t[0]}`] = addTiles(t[1], 'ega', nextTileCEGA, t[2]*4, t[3]*4, t[4]);
+			gfx[`cga-${t[0]}`] = addTiles(t[1], 'cga', nextTileCEGA, t[2]*2, t[3]*2, t[4]);
+			nextTileVGA += t[2];
+			nextTileCEGA += t[2]*2;
 		}
 
 		return {
